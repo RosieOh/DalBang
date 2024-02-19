6c62ee0a93c003c7204ee004b72145df804b358c
RosieOh <dhxogns920@gmail.com>
2024-02-19 17:19:58 +0900

    🏨build : ModelMapper 설정 및 의존성 주입 추가 #1🏨

--- src/main/java/com/dalbang/global/config/WebMvcConfig.java
+++ src/main/java/com/dalbang/global/config/WebMvcConfig.java
@@ -1,12 +1,28 @@
 package com.dalbang.global.config;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MatchingStrategy;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.ui.Model;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+                .setFieldMatchingEnabled(true)
+                .setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
+
+    }
 
     @Value("${uploadPath}")
     String uploadPath;
