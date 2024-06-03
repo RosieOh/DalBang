0dc1285c946bd4186f30dbf14adc2076e0fb7ff0
RosieOh <20172207@gm.hannam.ac.kr>
2024-06-03 16:58:49 +0900

    ✨CHORE : 멀티파트 파일 업로드 1대다 연관관계 매핑 및  비밀글, 최신 공지사항 추가로 인한 서비스 레이어 로직 수정 #4

--- src/main/java/com/dalbang/domain/board/service/BoardService.java
+++ src/main/java/com/dalbang/domain/board/service/BoardService.java
@@ -1,20 +1,36 @@
 package com.dalbang.domain.board.service;
 
 import com.dalbang.domain.board.dto.BoardDTO;
+import com.dalbang.domain.board.entity.Board;
+import com.dalbang.domain.file.entity.File;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface BoardService {
 
-    public BoardDTO findByBno(Long bno);
+    public BoardDTO findById(Long id);
 
-    public List<BoardDTO> findAll(BoardDTO boardDTO);
+    public List<Board> boardList();
 
-    public void register(BoardDTO boardDTO);
+    public Long register(BoardDTO boardDTO);
 
     public void modify(BoardDTO boardDTO);
 
-    public void remove(Long bno);
+    public void remove(Long id);
 
-    public List<BoardDTO>findByBoardType(String boardType);
+    public BoardDTO getBoard(Long id);
+
+    public List<BoardDTO> findByBoardType(String boardType);
+
+    // 메인페이지 최신 공지사항 5개 불러오기
+    public List<BoardDTO> newNoticeList();
+
+    public int countPinned(List<BoardDTO> boardDTOList);
+
+    // 검색 & 페이징
+    public Page<Board> searchBoard(String keyword, Integer id, Pageable pageable);
+    public int countPinnedPaging(Page<Board> pagingPinList);
+    public Board uploadFile(Long boardId, File file);
 }
--- src/main/java/com/dalbang/domain/board/service/BoardServiceImpl.java
+++ src/main/java/com/dalbang/domain/board/service/BoardServiceImpl.java
@@ -2,18 +2,27 @@ package com.dalbang.domain.board.service;
 
 import com.dalbang.domain.board.dto.BoardDTO;
 import com.dalbang.domain.board.entity.Board;
+import com.dalbang.domain.board.entity.QBoard;
 import com.dalbang.domain.board.repository.BoardRepository;
+import com.dalbang.domain.file.entity.File;
 import com.dalbang.global.constant.BoardType;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.QueryResults;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
+
 
 @Service
 @Log4j2
@@ -23,6 +32,7 @@ public class BoardServiceImpl implements BoardService{
 
     private final ModelMapper modelMapper;
     private final BoardRepository boardRepository;
+    private final JPAQueryFactory jpaQueryFactory;
 
     // 특정 게시판의 역할에 따라 타입별로 생성
     // 추가로 생성 시 따로 코드 추가 진행 필요
@@ -58,6 +68,54 @@ public class BoardServiceImpl implements BoardService{
     }
 
 
+    @Override
+    public BoardDTO findById(Long id) {
+        Optional<Board> result = boardRepository.findById(id);
+        BoardDTO boardDTO = modelMapper.map(result, BoardDTO.class);
+        return boardDTO;
+    }
+
+    @Override
+    public List<Board> boardList() {
+        return boardRepository.findAll();
+    }
+
+    @Override
+    public Long register(BoardDTO boardDTO) {
+        log.info(boardDTO.getBoardType());
+        Board board = Board.builder()
+                .id(boardDTO.getId())
+                .title(boardDTO.getTitle())
+                .content(boardDTO.getContent())
+                .boardType(boardDTO.getBoardType())
+                .writer(boardDTO.getWriter())
+                .pinned(boardDTO.isPinned())
+                .privated(boardDTO.isPrivated())
+                .build();
+        boardRepository.save(board);
+        return board.getId();
+    }
+
+    @Override
+    public void modify(BoardDTO boardDTO) {
+        Board board = modelMapper.map(boardDTO, Board.class);
+        board.change(boardDTO.getTitle(), boardDTO.getContent(), boardDTO.isPinned(), boardDTO.isPrivated());
+        boardRepository.save(board);
+    }
+
+    @Override
+    public void remove(Long id) {
+        boardRepository.deleteById(id);
+    }
+
+    @Override
+    public BoardDTO getBoard(Long id) {
+        Optional<Board> result = boardRepository.findById(id);
+        Board board = result.orElseThrow();
+        BoardDTO boardDTO = modelMapper.map(board, BoardDTO.class);
+        return boardDTO;
+    }
+
     // boardType에 따라 자동으로 게시판이 분리되는 역할
     @Override
     public List<BoardDTO> findByBoardType(String boardType) {
@@ -66,60 +124,88 @@ public class BoardServiceImpl implements BoardService{
         if (boardList != null && !boardList.isEmpty()) {
             List<BoardDTO> boardDTOList = boardList.stream().map(board -> modelMapper.map(board, BoardDTO.class))
                     .collect(Collectors.toList());
+            return boardDTOList;
+        } else {
+            return Collections.emptyList();
         }
-        return null;
     }
 
     @Override
-    public BoardDTO findByBno(Long bno) {
-        Optional<Board> result = boardRepository.findById(bno);
-        BoardDTO boardDTO =modelMapper.map(result, BoardDTO.class);
-        return boardDTO;
-    }
+    public List<BoardDTO> newNoticeList() {
+        List<Board> noticesList = boardRepository.newNoticeList();
 
-    @Override
-    public List<BoardDTO> findAll(BoardDTO boardDTO) {
-        List<Board> boardList = boardRepository.findAll();
-        List<BoardDTO> boardDTOList = new ArrayList<>();
-
-        for (Board board1 : boardList) {
-            Board board = Board.builder()
-                    .bno(boardDTO.getBno())
-                    .title(boardDTO.getTitle())
-                    .content(boardDTO.getContent())
-                    .boardType(boardDTO.getBoardType())
-                    .writer(boardDTO.getWriter())
-                    .fileId(boardDTO.getFileId())
-                    .build();
-            boardDTOList.add(boardDTO);
+        // entity 내용 출력
+        for (Board notice : noticesList) {
+            log.info("Notice 게시물 출력 : " + notice.getTitle());
         }
-        return boardDTOList;
+        List<BoardDTO> newNoticeList = noticesList.stream()
+                .map(board -> modelMapper.map(board, BoardDTO.class))
+                .collect(Collectors.toList());
+        return newNoticeList;
+    }
+
+    // 고정 게시물 추가 기능
+    @Override
+    public int countPinned(List<BoardDTO> boardDTOList) {
+        int pinnedCount = 0;
+        for (BoardDTO boardDTO : boardDTOList) {
+            if (boardDTO.isPinned())
+                pinnedCount++;
+        }
+        return pinnedCount;
     }
 
     @Override
-    public void register(BoardDTO boardDTO) {
+    public Page<Board> searchBoard(String keyword, Integer id, Pageable pageable) {
+        BooleanBuilder where = new BooleanBuilder();
 
-        Board board = Board.builder()
-                .bno(boardDTO.getBno())
-                .title(boardDTO.getTitle())
-                .content(boardDTO.getContent())
-                .writer(boardDTO.getWriter())
-                .boardType(boardDTO.getBoardType())
-                .fileId(boardDTO.getFileId())
-                .build();
-        boardRepository.save(board);
+        // 키워드가 있는 경우 이름 필터링
+        if (StringUtils.hasText(keyword)) {
+            where.and(QBoard.board.title.containsIgnoreCase(keyword));
+        }
+
+
+        // boardType이 Class_Notice인 것만 필터링
+        where.and(QBoard.board.boardType.eq(String.valueOf(BoardType.NOTICE)));
+        // deleteType이 false인 애들만 필터링 (삭제되지 않은 게시물)
+        where.and(QBoard.board.deleteType.eq(false));
+
+        // 페이징 처리
+        JPAQuery<Board> query = jpaQueryFactory
+                .selectFrom(QBoard.board)
+                .where(where)
+                .orderBy(QBoard.board.pinned.desc(), QBoard.board.createdTime.desc());
+
+        // 페이징 처리된 결과 반환
+        QueryResults<Board> results = query
+                .offset(pageable.getOffset()) // 오프셋 설정
+                .limit(pageable.getPageSize()) // 페이지 크기 설정
+                .fetchResults(); // 결과 가져오기
+
+        return new PageImpl<>(results.getResults(), pageable, results.getTotal());
     }
 
     @Override
-    public void modify(BoardDTO boardDTO) {
-        Optional<Board> result = boardRepository.findById(boardDTO.getBno());
-        Board board = result.orElseThrow();
-        board.change(boardDTO.getTitle(), boardDTO.getContent());
-        boardRepository.save(board);
+    public int countPinnedPaging(Page<Board> pagingPinList) {
+        int PinnedPaging = 0;
+        for (Board board : pagingPinList) {
+            if (board.isPinned())
+                PinnedPaging++;
+        }
+        return PinnedPaging;
     }
 
+    // 다중 업로드
     @Override
-    public void remove(Long bno) {
-        boardRepository.deleteById(bno);
+    public Board uploadFile(Long boardId, File file) {
+        Optional<Board> optionalBoard = boardRepository.findById(boardId);
+        if (optionalBoard.isPresent()) {
+            Board board = optionalBoard.get();
+            file.setBoard(board);
+            board.getFiles().add(file);
+            return boardRepository.save(board);
+        } else {
+            throw new IllegalArgumentException("Board not found with id: " + boardId);
+        }
     }
 }
