0766218a13d32cde94971d3760a33a733ec5a124
RosieOh <20172207@gm.hannam.ac.kr>
2024-06-03 16:59:50 +0900

    ✨FEAT : Board 게시판 다대일 연관관계 매핑으로 인한 비즈니스 로직 구현 #5

--- src/main/java/com/dalbang/domain/file/repository/FileRepository.java
+++ src/main/java/com/dalbang/domain/file/repository/FileRepository.java
@@ -2,7 +2,16 @@ package com.dalbang.domain.file.repository;
 
 import com.dalbang.domain.file.entity.File;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface FileRepository extends JpaRepository<File, Long > {
+    @Query("SELECT f FROM File f JOIN f.board b WHERE b.id = :id")
+    List<File> findByBoardId(Long id);
 
+    @Query(value = "SELECT id FROM file WHERE board_id = :boardId", nativeQuery = true)
+    List<Long> findIdsByBoardId(Long boardId);
 }
