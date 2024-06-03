8c4f99460de416a83dc1fdcb36a80d5747937590
RosieOh <20172207@gm.hannam.ac.kr>
2024-06-03 16:58:33 +0900

    ✨CHORE : 멀티파트 파일 업로드 1대다 연관관계 매핑 및  비밀글, 최신 공지사항 추가로 인한 비즈니스 로직 수정 #4

--- src/main/java/com/dalbang/domain/board/repository/BoardRepository.java
+++ src/main/java/com/dalbang/domain/board/repository/BoardRepository.java
@@ -5,18 +5,36 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface BoardRepository extends JpaRepository<Board, Long> {
+public interface BoardRepository extends JpaRepository<Board, Long>, QuerydslPredicateExecutor<Board> {
+    /*
+    QuerydslPredicateExecutor<Entity>
+    -> Dynamic QueryDSL을 활용할 때 사용
+     */
 
-    @Query("SELECT b FROM Board b WHERE b.bno = :bno")
-    Optional<Board> findById(@Param("bno") Long bno);
+    @Query("select b from Board b where b.id = :id")
+    Optional<Board> findById(@Param("id") Long id);
 
-    @Query("SELECT b from Board b where b.boardType = :boardType")
+    @Query("select b from Board b where b.boardType = :boardType")
     List<Board> findByBoardType(@Param("boardType") String boardType);
 
-    Page<Board> findByBoardType(String boardType, Pageable pageable);
+    @Query("select b from Board b where b.writer = :writer")
+    Optional<Board> findByName(@Param("writer") String writer);
+
+    Page<Board> findByBoardType(Pageable pageable, String boardType);
+
+    // 메인 페이지 최신 공지사항 5개 불러오기
+    @Query("select b from Board b where b.id = 0 order by b.pinned desc, b.createdTime desc LIMIT 5")
+    List<Board> newNoticeList();
+
+    // 고정 게시글 상위 조회 + 최신 작성순
+    @Query("select b from Board b order by b.pinned desc, b.createdTime desc")
+    List<Board> findAll();
+
+
 }
