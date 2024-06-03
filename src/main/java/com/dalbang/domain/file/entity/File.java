41e2280a83a83e78e3984e8716d6b3ebf3b5a227
RosieOh <20172207@gm.hannam.ac.kr>
2024-06-03 16:59:27 +0900

    ✨CHORE : Board 게시판 다대일 연관관계 매핑으로 인한 Entity 로직 수정 #5

--- src/main/java/com/dalbang/domain/file/entity/File.java
+++ src/main/java/com/dalbang/domain/file/entity/File.java
@@ -1,15 +1,11 @@
 package com.dalbang.domain.file.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import com.dalbang.domain.board.entity.Board;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Getter
+@Setter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class File {
@@ -27,11 +23,30 @@ public class File {
     @Column(nullable = false)
     private String fileName;
 
+    @ManyToOne
+    @JoinColumn(name = "board_id")
+    private Board board;
+
     @Builder
     public File(Long id, String originFileName, String filePath, String fileName) {
         this.id = id;
         this.originFileName = originFileName;
         this.fileName = fileName;
         this.filePath = filePath;
+        this.board = board;
+    }
+
+    public File(String originFileName, String fileName, String filePath, Board board) {
+        this.originFileName = originFileName;
+        this.filePath = filePath;
+        this.fileName = fileName;
+        this.board = board;
+    }
+
+    public void change(Long id, String fileName, String filePath, String originFileName) {
+        this.id = id;
+        this.filePath = filePath;
+        this.originFileName = originFileName;
+        this.fileName = fileName;
     }
 }
