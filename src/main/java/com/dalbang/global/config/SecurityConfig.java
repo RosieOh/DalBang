7f306a9d044dd32f41f194fb463adcfaf8ebbadb
RosieOh <20172207@gm.hannam.ac.kr>
2024-06-03 17:02:35 +0900

    ✨CHORE : 클라이언트 연결 시 Cors 에러 관리 Security 설정 연동#8

--- src/main/java/com/dalbang/global/config/SecurityConfig.java
+++ src/main/java/com/dalbang/global/config/SecurityConfig.java
@@ -1,76 +1,69 @@
 package com.dalbang.global.config;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.session.SessionRegistry;
-import org.springframework.security.core.session.SessionRegistryImpl;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
 
 @Log4j2
 @Configuration
+@RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final CorsConfig corsConfig;
+
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-                // stateless한 rest api를 개발할 것이므로 csrf 공격에 대한 옵션은 꺼둔다.
-//                .csrf(AbstractHttpConfigurer::disable)
-//                .cors(AbstractHttpConfigurer::disable)
-                // 특정 URL에 대한 권한 설정.
-                .authorizeHttpRequests((authorizeRequests) -> {
-                    authorizeRequests
-                            .requestMatchers("/video/**").permitAll()
-                            .requestMatchers("/user/**").authenticated() // 인증된, 로그인 한 사람만 접근 가능
-                            .requestMatchers("/admin/**").hasAuthority("ADMIN") // admin만 접근 가능
-                            .requestMatchers("/css/**", "/js/**", "/upload/**", "/cleditor/**", "/scss/**",
-                                    "/vendors/**", "/ckeditor/**", "/webfonts/**", "/resource/**", "/assets/**")
-                            .permitAll() // 모두 접근 가능
-                            .anyRequest().permitAll();
+        log.info("-------------------  filter Chain Start ------------------");
+
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authorizeRequests) -> {authorizeRequests
+                        .requestMatchers(new AntPathRequestMatcher("/")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/member/**"), new AntPathRequestMatcher("/notice/**")).authenticated()
+                        .requestMatchers(new AntPathRequestMatcher("/admin/**")).hasAnyAuthority("ADMIN", "MANAGER")
+                        .anyRequest().permitAll();
                 })
-                // 로그인
-                .formLogin((formLogin) -> {
-                    formLogin
-                            .loginPage("/login")
-                            .loginProcessingUrl("/loginPro")
-                            .failureUrl("/login?error=true")
-                            .defaultSuccessUrl("/status")
-                            .usernameParameter("id")
-                            .passwordParameter("pw")
-                    ;
-                })
-                // 로그아웃
-                .logout((logout) -> {
-                    logout
-                            .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                            .invalidateHttpSession(true)
-                            .logoutSuccessUrl("/");
-                })
-                // 인증 관련 예외, 사용하면 에러페이지 발생해서 주석처리
+                .formLogin((formLogin) -> formLogin
+                        .loginPage("/")
+                        .failureUrl("/?error=true")
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/member/status")
+                        .usernameParameter("id")
+                        .passwordParameter("pw"))
+
+                .logout((logout) -> logout.logoutUrl("/logout").logoutSuccessUrl("/"))
+                .addFilter(corsConfig.corsFilter())     // CorsFilter 등록
                 .exceptionHandling((exceptionHandling) -> {
-                    exceptionHandling.authenticationEntryPoint(new CustomAuthenticationEntryPoint());
+                    exceptionHandling
+                            .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+                            .accessDeniedPage("/denied") // 접근 거부 페이지 설정
+                            .defaultAuthenticationEntryPointFor(new Http403ForbiddenEntryPoint(), new AntPathRequestMatcher("/error/500")); // 500 에러 페이지 설정
                 })
-                // 중복 로그인 방지
-                .sessionManagement((sessionManagement) -> {
-                    sessionManagement.sessionFixation().changeSessionId()
-                            .maximumSessions(1)
-                            .maxSessionsPreventsLogin(false)
-                            .sessionRegistry(sessionRegistry());
-                })
-                .build();
+
+                .headers((headers) -> headers.addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)));
+        log.info("-------------------  filter Chain End ------------------");
+
+        return http.build();
     }
+
     @Bean
-    public SessionRegistry sessionRegistry() {
-        return new SessionRegistryImpl();
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        log.info("-------------------- WebSecurity ----------------------");
+        return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+
 }
